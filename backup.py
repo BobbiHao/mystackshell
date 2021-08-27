db196e8c5e25c99de701249098e3c7079566c67e
BobbiHao <bobhao_123@163.com>
2021-08-27 08:56:42 +0800

    backup.py

--- backup.py
+++ backup.py
@@ -1,21 +1,52 @@
 #!/usr/bin/python3
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 import time
 import os
-import sys
 import string
 import socket
+import contextlib
+import ftplib
+
+
 global INI_FILE
-INI_FILE = "backup.ini"
+INI_FILE = os.getcwd() + '/backup.ini'
 global REDIS_TMP
-REDIS_TMP = "/tmp/redis-save"
+REDIS_TMP = '/tmp/redis-save'
 dic = {}
+
 daytime = time.strftime("%Y-%m-%d", time.localtime())
 datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 print("daytime is ", daytime)
 print("datetime is ", datetime)
+
+class MyFTP(ftplib.FTP):
+    def mkpath(self, dirpath):
+        head, tail = os.path.split(dirpath)
+        if not tail:
+            head, tail = os.path.split(head)
+        if head and tail and not os.path.exists(head):
+            self.mkpath(head)
+        try:
+            self.mkd(dirpath)
+        except:
+            pass
+    def ftp_check_and_mkpath(self, dirpath) -> int:
+        try:
+            self.cwd(dirpath)
+        except Exception as e:
+            logging.debug(e)
+            try:
+                self.mkpath(dirpath)
+            except Exception as e:
+                logging.debug(e)
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+
 
 def get_installcmd():
     os_VENDOR = os.popen("lsb_release -i -s").read().strip('\n')
@@ -109,7 +140,7 @@ def add_dic_from_ini():
     logging.info("INIT DIC FROM INI END")
 
 
-def save_redis():
+def save_redis() -> int:
     redis_values = get_value('redis', '').split('\n')
     logging.debug("redis_values is %s" % redis_values)
     if not redis_values:
@@ -131,16 +162,12 @@ def save_redis():
         PASS = ''
     logging.debug("PASS is %s" % PASS)
 
-    a = os.system("redis-cli %s > /dev/null << EOF"
-              "CONFIG SET dir %s \n"
-              "SAVE \n"
-"EOF" % (PASS, REDIS_TMP))
-    if a == 0:
-        logging.info("redis data save succeed...")
-    else:
+    if os.system("redis-cli %s CONFIG SET dir %s > /dev/null" % (PASS, REDIS_TMP)) != 0:
+        return 1
+    if os.system("redis-cli SAVE > /dev/null") != 0:
         logging.warning("redis data save failed!!!")
         return 1
-
+    logging.info("redis data save succeed...")
     return 0
 
 
@@ -162,84 +189,174 @@ def create_mysql():
                         "UNIQUE INDEX uuid_etc_bakup_UNIQUE(uuid ASC) VISIBLE);"
 
     password = get_value('conf', 'mysql_password')
-    if os.system("mysql -u root -e %s -p%s &>/dev/null" % (create_table_sql, password)) != 0:
+    logging.info("mysql -u root -e '%s' -p'%s' >/dev/null" % (create_table_sql, password))
+    if os.system("mysql -u root -e '%s' -p'%s' >/dev/null" % (create_table_sql, password)) != 0:
         exit(1)
 
 def splice_tarfilename(servername, srcpath, data_type) -> string:
     _datetime = datetime.replace(' ', '_')
     srcpath_changed = srcpath.replace('/', '_')
 
-    filename = socket.gethostname().join('_').join(_datetime).join('_').join(servername)
+    filename = "%s_%s_%s" % (socket.gethostname(), _datetime, servername)
     while filename[-1] == '_':
         filename=filename[:-1]
-    filename.join(srcpath_changed)
+    filename += srcpath_changed
     while filename[-1] == '_':
         filename=filename[:-1]
-    filename.join('_').join(data_type).join('.tar.gz')
-
+    filename = "%s_%s.tar.gz" % (filename, data_type)
     return filename
 
-def mytar_to_somewhere():
+
+@contextlib.contextmanager
+def pushd(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
+
+def get_a_insert_record(key) -> dict:
+    oneline = dict()
+    oneline['srcpath'] = key
+    oneline['unit'] = dic[key]['unit']
+    oneline['data_type'] = dic[key]['data_type']
+    oneline['backup_dst'] = dic[key]['backup_dst']
+    oneline['backup_remotedst'] = dic[key]['backup_remotedst']
+    oneline['backup_period'] = dic[key]['backup_period']
+    oneline['backup_volume'] = dic[key]['backup_volume']
+    oneline['save_time'] = dic[key]['save_time']
+    oneline['uuid'] = os.popen("uuidgen").read().strip('\n')
+    oneline['filename'] = splice_tarfilename(oneline['unit'], oneline['srcpath'], oneline['data_type'])
+    logging.debug("srcpath: %s, unit: %s, data_type: %s, backup_dst: %s, backup_remotedst: %s, backup_period: %s, "
+                  "backup_volume: %s, save_time: %s" % (oneline['srcpath'], oneline['unit'], oneline['data_type'], oneline['backup_dst'],
+                    oneline['backup_remotedst'], oneline['backup_period'], oneline['backup_volume'], oneline['save_time']))
+    return oneline
+
+def insert_a_record_tomysql(record) -> int:
+    uuid = record['uuid']
+    servername = record['unit']
+    filename = record['filename']
+    srcpath = record['srcpath']
+    backup_dst = record['backup_dst']
+    backup_remotedst = record['backup_remotedst']
+    backup_period = record['backup_period']
+    backup_volume = record['backup_volume']
+    save_time = record['save_time']
+
+    add_table_sql = "insert into backup.etc_backup values(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");" % (
+    uuid, servername, filename, srcpath, backup_dst, backup_remotedst, datetime, backup_period, backup_volume, save_time)
+
+    logging.info("add table sql is '%s'" % add_table_sql)
+    password = get_value('conf', 'mysql_password')
+    logging.debug("password is %s" % password)
+    logging.info("mysql -u root -e '%s' -p%s > /dev/null" % (add_table_sql, password))
+    return os.system("mysql -u root -e '%s' -p'%s' >/dev/null" % (add_table_sql, password))
+
+def tar_to_somewhere(srcpath, dstpath) -> int:
+    if os.path.isdir(srcpath):
+        srcpath_s_dir = srcpath
+    else:
+        srcpath_s_dir = os.path.dirname(srcpath)
+    with pushd(srcpath_s_dir):
+        logging.debug("pushd in %s, now cwd is %s" % (srcpath_s_dir, os.getcwd()))
+        if os.path.isdir(srcpath):
+            logging.info("tar -czvf %s %s/*" % (dstpath, srcpath))
+            tarres = os.system("tar -czvf %s ./*" % dstpath)
+        else:
+            logging.info("tar -czvf %s %s" % (dstpath, srcpath))
+            tarres = os.system("tar -czvf %s %s" % (dstpath, os.path.basename(srcpath)))
+    return tarres
+
+def mylocal_to_somewhere():
     for key in dic.keys():
         logging.debug("key is %s" % key)
-        srcpath = key
-        servername = dic[key]['unit']
-        data_type = dic[key]['data_type']
-        backup_dst = dic[key]['backup_dst']
-        backup_remotedst = dic[key]['backup_remotedst']
-        backup_period = dic[key]['backup_period']
-        backup_volume = dic[key]['backup_volume']
-        save_time = dic[key]['save_time']
-
-        logging.debug("servername: %s, data_type: %s, backup_dst: %s, backup_remotedst: %s, backup_period: %s, "
-                      "backup_volume: %s, save_time: %s" % (servername, data_type, backup_dst, backup_remotedst,
-                                                            backup_period,backup_volume, save_time))
-        uuid = os.popen("uuidgen").read().strip('\n')
-        filename = splice_tarfilename(servername, srcpath, data_type)
-        if os.path.exists(srcpath) == False:
-            logging.warning("in mytar_to_somewhere: %s is not exist, skip" % srcpath)
+        if os.path.exists(key) == False:
+            logging.warning("in mylocal_to_somewhere: %s is not exist, skip" % key)
             continue
 
-        check_and_mkpath(backup_dst)
+        a_insert_record = get_a_insert_record(key)
 
-        if os.path.isdir(srcpath):
-            os.system("pushd %s > /dev/null" % srcpath)
-            logging.info("tar -czvf %s/%s %s/*" % (backup_dst, filename, srcpath))
-            tarres = os.system("tar -czvf %s/%s %s/*" % (backup_dst, filename, srcpath))
-        else:
-            os.system("pushd %s/.. > /dev/null" % srcpath)
-            logging.info("tar -czvf %s/%s %s" % (backup_dst, filename, srcpath))
-            tarres = os.system("tar -czvf %s/%s %s" % (backup_dst, filename, srcpath))
+        check_and_mkpath(a_insert_record['backup_dst'])
+
+        tarres = tar_to_somewhere(key, a_insert_record['backup_dst'] + "/" + a_insert_record['filename']);
         if tarres == 0 or tarres == 1:
-            if srcpath == REDIS_TMP:
-                srcpath = 'redis'
-            add_table_sql = "insert into backup.etc_backup values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);" % (uuid, servername,
-                filename, srcpath, backup_dst, backup_remotedst, datetime, backup_period, backup_volume, save_time)
-            logging.info("add table sql is %s" % add_table_sql)
-            if os.system("mysql -u root -e %s -p %s >/dev/null") == False:
+            if key == REDIS_TMP:
+                a_insert_record['srcpath'] = 'redis'
+            if insert_a_record_tomysql(a_insert_record) != 0:
                 exit(1)
-        os.system("popd > /dev/null")
 
+def myscp_to_somewhere():
+    print("this is myscp_to_somewhere")
+
+
+def myftp_to_somewhere():
+    print("this is myftp_to_somewhere")
+
+    ftp_ip = get_value('ftp', 'ip')
+    remote_user = get_value('ftp', 'user')
+    remote_password = get_value('ftp', 'password')
+    logging.info("ip is %s, remote_user is %s, remote_password is %s" % (ftp_ip, remote_user, remote_password))
+
+    with MyFTP(ftp_ip) as ftp:
+        try:
+            ftp.login(remote_user, remote_password)
+            _myftp_to_somewhere(ftp)
+        except ftplib.all_errors as e:
+            logging.error("in myftp_to_somewhere: %s" %e)
+
+
+def _myftp_to_somewhere(ftp):
+
+    for key in dic.keys():
+        logging.debug("key is %s" % key)
+        if os.path.exists(key) == False:
+            logging.warning("in mylocal_to_somewhere: %s is not exist, skip" % key)
+            continue
+
+        a_insert_record = get_a_insert_record(key)
+        backup_remotedst = a_insert_record['backup_remotedst']
+        if ftp.ftp_check_and_mkpath(backup_remotedst) != 0:
+            logging.error("remote dstdir %s make failed, skip" % backup_remotedst)
+            continue
+
+        tmpdir = '/tmp/%s' % a_insert_record['unit']
+        check_and_mkpath(tmpdir)
+        os.system("rm -rf %s/*" % tmpdir)
+
+        srcfilepath = tmpdir + "/" + a_insert_record['filename']
+        tarres = tar_to_somewhere(key, srcfilepath)
+        if tarres == 0 or tarres == 1:
+            with open(srcfilepath, 'rb') as fp:
+                res = ftp.storbinary("STOR " + backup_remotedst + "/" + a_insert_record['filename'], fp)
+                if not res.startswith('226 Transfer complete'):
+                    logging.error('%s Upload failed', srcfilepath)
+                    continue
+            if key == REDIS_TMP:
+                a_insert_record['srcpath'] = 'redis'
+            if insert_a_record_tomysql(a_insert_record) != 0:
+                exit(1)
+        else:
+            logging.error("key %s not insert to database, skipped!" % key)
 
 if __name__ == "__main__":
-    install = get_installcmd()
-    print("APT is %s" %install)
 
-    # get_package('haha', 'haha')
+    if os.getuid() != 0:
+        logging.error("please use root privileges")
+        exit(1)
 
-    backup_or_restore = get_value('conf', 'backup_or_restore')
-    print("backup_or_restore is ", backup_or_restore)
-
-    sections = get_section()
-    print("sections is ", sections)
-
-    print("add_dic_from_ini")
-    add_dic_from_ini()
-
-    print(str(dic))
+    get_package('crudini', 'crudini')
 
     save_redis()
 
-#    for key in dic.items():
-#        print("%s: " %key)
-    mytar_to_somewhere()
+    create_mysql()
+    add_dic_from_ini()
+
+    opers = dict()
+    opers['local'] = mylocal_to_somewhere
+    opers['scp'] = myscp_to_somewhere
+    opers['ftp'] = myftp_to_somewhere
+    backup_type = get_value('conf', 'backup_type')
+
+    opers[backup_type]()
+    logging.info("succeed, good bye")
